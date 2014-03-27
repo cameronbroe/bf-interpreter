3fd5198b50915e99afb00ee04267d27dfda52fb0
CommanderViral <cameron.b.roe@gmail.com>
2014-03-27 15:01:23 -0500

    Integrate syntax checker

---
+++ syntax.h
@@ -0,0 +1,12 @@
+/*	Header file for the syntax checker
+	Function that goes through and checks the syntax of the code
+*/
+#ifndef SYNTAX_H_FOUND
+#define SYNTAX_H_FOUND
+
+#include <string>
+#include "syntax.h"
+
+int check_syntax(std::string code);
+
+#endif
